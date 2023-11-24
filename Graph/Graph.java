3b903848729b977270009d99ecbb4904816629ab
Trustacean <pizzacrustacean15@gmail.com>
2023-11-25 00:23:09 +0700

    25/11/23

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -1,17 +1,22 @@
 package Graph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Stack;
 import java.util.Queue;
 
 public class Graph {
     ArrayList<ArrayList<Integer>> adjMatrix;
     ArrayList<Vertex> vertexList;
+    ArrayList<Edge> edgeList;
 
     public Graph() {
         vertexList = new ArrayList<Vertex>();
         adjMatrix = new ArrayList<ArrayList<Integer>>();
+        edgeList = new ArrayList<Edge>();
     }
 
     public int size() {
@@ -42,26 +47,33 @@ public class Graph {
 
     }
 
-    public void addEdge(int source, int target) {
+    public void addEdge(int source, int target, int weight) {
         // perbarui relasi antar vertex dalam matriks
-        adjMatrix.get(source).set(target, 1);
-        adjMatrix.get(target).set(source, 1);
+        adjMatrix.get(source).set(target, weight);
+        adjMatrix.get(target).set(source, weight);
+        edgeList.add(new Edge(source, target, weight));
     }
 
     public void display() {
         System.out.print("~ ");
         for (int i = 0; i < adjMatrix.size(); i++) {
-            System.out.print(vertexList.get(i).getLabel() + " ");
+            System.out.print(String.format("%3s", vertexList.get(i).getLabel()));
         }
 
         System.out.println();
 
         for (int row = 0; row < adjMatrix.size(); row++) {
-            System.out.print(vertexList.get(row).getLabel() + " ");
+            System.out.print(String.format("%-3s", vertexList.get(row).getLabel()));
             for (int column = 0; column < adjMatrix.size(); column++) {
-                System.out.print(adjMatrix.get(row).get(column) + " ");
+                System.out.print(String.format("%3s", adjMatrix.get(row).get(column) + " "));
             }
             System.out.println();
+        }
+    }
+
+    void setAllVisitedToFalse() {
+        for (int i = 0; i < adjMatrix.size(); i++) {
+            vertexList.get(i).visited = false;
         }
     }
 
@@ -69,17 +81,17 @@ public class Graph {
         Stack<Vertex> stack = new Stack<Vertex>();
         stack.push(vertexList.get(0));
         vertexList.get(0).visited = true;
-        System.out.println(vertexList.get(0).getLabel() + " ");
+        System.out.print(vertexList.get(0).getLabel() + " ");
 
         while (!stack.isEmpty()) {
             Vertex current = stack.peek();
             boolean allAdjVisited = true;
 
             for (int i = 0; i < adjMatrix.size(); i++) {
-                if (adjMatrix.get(vertexList.indexOf(current)).get(i) == 1 && !vertexList.get(i).visited) {
+                if (adjMatrix.get(vertexList.indexOf(current)).get(i) != 0 && !vertexList.get(i).visited) {
                     stack.push(vertexList.get(i));
                     vertexList.get(i).visited = true;
-                    System.out.println(vertexList.get(i).getLabel() + " ");
+                    System.out.print(vertexList.get(i).getLabel() + " ");
                     allAdjVisited = false;
                     break;
                 }
@@ -89,40 +101,94 @@ public class Graph {
                 stack.pop();
             }
         }
-
-        for (int i = 0; i < adjMatrix.size(); i++) {
-            vertexList.get(i).visited = false;
-        }
+        setAllVisitedToFalse();
+        System.out.println();
     }
 
     public void bfs() {
         Queue<Vertex> queue = new LinkedList<Vertex>();
         queue.add(vertexList.get(0));
         vertexList.get(0).visited = true;
-        System.out.println(vertexList.get(0).getLabel() + " ");
+        System.out.print(vertexList.get(0).getLabel() + " ");
 
         while (!queue.isEmpty()) {
             Vertex current = queue.poll();
 
             for (int i = 0; i < adjMatrix.size(); i++) {
-                if (adjMatrix.get(vertexList.indexOf(current)).get(i) == 1 && !vertexList.get(i).visited) {
+                if (adjMatrix.get(vertexList.indexOf(current)).get(i) != 0 && !vertexList.get(i).visited) {
                     queue.add(vertexList.get(i));
                     vertexList.get(i).visited = true;
-                    System.out.println(vertexList.get(i).getLabel() + " ");
+                    System.out.print(vertexList.get(i).getLabel() + " ");
                 }
             }
         }
 
-        for (int i = 0; i < adjMatrix.size(); i++) {
-            vertexList.get(i).visited = false;
-        }
+        setAllVisitedToFalse();
+        System.out.println();
     }
+
+    int getMinimumVertex(boolean[] mst, int[] key) {
+        int minKey = Integer.MAX_VALUE;
+        int vertex = -1;
+        for (int i = 0; i < vertexList.size(); i++) {
+            // Neighboring vertex havent visited and the weight is lesser
+            if (mst[i] == false && minKey > key[i]) {
+                minKey = key[i];
+                vertex = i;
+            }
+        }
+        // Returning the vertex with smallest edge weight
+        return vertex;
+    }
+
+    public void primMST() {
+        boolean[] mst = new boolean[vertexList.size()];
+        int[] key = new int[vertexList.size()];
+        Edge[] mstEdges = new Edge[vertexList.size() - 1];
+
+        Arrays.fill(key, Integer.MAX_VALUE);
+
+        key[0] = 0;
+
+        for (int i = 0; i < vertexList.size() - 1; i++) {
+            int vertex = getMinimumVertex(mst, key);
+            mst[vertex] = true;
+
+            for (int j = 0; j < vertexList.size(); j++) {
+                if (adjMatrix.get(vertex).get(j) > 0 && !mst[j] && adjMatrix.get(vertex).get(j) < key[j]) {
+                    mstEdges[j - 1] = new Edge(vertex, j, adjMatrix.get(vertex).get(j));
+                    key[j] = adjMatrix.get(vertex).get(j);
+                }
+            }
+        }
+
+        Arrays.sort(mstEdges, Comparator.comparingInt(edge -> (edge != null) ? edge.vertex1 : Integer.MAX_VALUE));
+
+        printPrimMST(mstEdges);
+    }
+
+    void printPrimMST(Edge[] primMst) {
+        int totalMinWeight = 0;
+        System.out.println("Minimum Spanning Tree: ");
+
+        for (int i = 0; i < primMst.length; i++) {
+            if (primMst[i] != null) {
+                System.out.println(
+                        "Edge: " + vertexList.get(primMst[i].vertex1).getLabel() +
+                                " - " + vertexList.get(primMst[i].vertex2).getLabel() +
+                                " Weight: " + primMst[i].weight);
+                totalMinWeight += primMst[i].weight;
+            }
+        }
+        System.out.println("Total minimum weight: " + totalMinWeight);
+    }
+
+    
 
     public static void main(String[] args) {
         Graph theGraph = new Graph();
         theGraph.addVertex("A"); // 0
         theGraph.addVertex("B"); // 1
-
         theGraph.addVertex("C"); // 2
         theGraph.addVertex("D"); // 3
         theGraph.addVertex("E"); // 4
@@ -130,34 +196,42 @@ public class Graph {
         theGraph.addVertex("G"); // 6
         theGraph.addVertex("H"); // 7
         theGraph.addVertex("I"); // 8
+        theGraph.addVertex("J"); // 9
         System.out.println("Daftar Vertex : ");
         for (int i = 0; i < theGraph.size(); i++) {
-            System.out.println(theGraph.displayVertex(i));
+            System.out.print(theGraph.displayVertex(i) + " ");
         }
-        theGraph.addEdge(0, 1); // AB
-        theGraph.addEdge(0, 5); // AF
-        theGraph.addEdge(0, 8); // AI
-        theGraph.addEdge(1, 4); // BE
-        theGraph.addEdge(1, 2); // BC
-        theGraph.addEdge(2, 3); // CD
-        theGraph.addEdge(2, 4); // CE
-        theGraph.addEdge(3, 6); // DG
-        theGraph.addEdge(3, 7); // DH
-        theGraph.addEdge(6, 4); // GE
-        theGraph.addEdge(6, 5); // GF
+        System.out.println();
+
+        theGraph.addEdge(0, 1, 5); // AB
+        theGraph.addEdge(0, 2, 5); // AC
+        theGraph.addEdge(1, 3, 4); // BD
+        theGraph.addEdge(1, 4, 9); // BE
+        theGraph.addEdge(2, 3, 4); // CD
+        theGraph.addEdge(2, 5, 7); // CF
+        theGraph.addEdge(3, 4, 8); // DE
+        theGraph.addEdge(3, 5, 6); // DF
+        theGraph.addEdge(4, 5, 10); // EF
+        theGraph.addEdge(4, 6, 10); // EG
+        theGraph.addEdge(4, 7, 8); // EH
+        theGraph.addEdge(4, 8, 9); // EI
+        theGraph.addEdge(5, 6, 3); // FG
+        theGraph.addEdge(6, 7, 6); // GH
+        theGraph.addEdge(6, 9, 7); // GJ
+        theGraph.addEdge(7, 8, 4); // HI
+        theGraph.addEdge(7, 9, 6); // HJ
+        theGraph.addEdge(8, 9, 7); // IJ
 
         System.out.println("Matrik adjacency");
         theGraph.display();
-        System.out.println("\nDFS");
-        theGraph.dfs();
-        System.out.println("\nBFS");
-        theGraph.bfs();
+        theGraph.primMST();
+
     }
 }
 
 class Vertex {
-    public String label;
-    public Boolean visited;
+    String label;
+    Boolean visited;
 
     public Vertex(String label) {
         this.label = label;
@@ -169,5 +243,17 @@ class Vertex {
 
     public String toString() {
         return label;
+    }
+}
+
+class Edge {
+    int vertex1;
+    int vertex2;
+    int weight;
+
+    public Edge(int vertex1, int vertex2, int weight) {
+        this.vertex1 = vertex1;
+        this.vertex2 = vertex2;
+        this.weight = weight;
     }
 }
